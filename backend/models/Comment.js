81666375c2eeea29014b96257aada3379076fc9c
Behlim Shaeem <behlimsahim@gmail.com>
2025-06-11 16:23:21 +0530

    add comment model

---
+++ backend/models/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    parentComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    }, // for threaded replies
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BlogPost",
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
