7c6b918c9df3e396463080c1826ce0e260d10adb
Behlim Shaeem <behlimsahim@gmail.com>
2025-06-11 16:18:34 +0530

    add user model

---
+++ backend/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true },
+    bio: { type: String, default: "" }, // Optional short bio
+  },
+  { timestamps: true }
+);
+
+// Hash password hook
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  }
+});
+
+// Compare password method
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generate token method
+UserSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
